e0aadd40321580fcf3eb4d0399c01ad5e11f1491
AbuSameer <abusameer967@gmail.com>
2026-03-14 20:50:50 +0530

    update

--- app.py
+++ app.py
@@ -678,6 +678,6 @@ def youtube_analysis():
 init_db()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
     # deploy update
